550898d6e8d7c3136b7439a6791e8e638b84a427
Lev Khruschov <baronfontarkio@gmail.com>
2025-12-09 15:42:11 +0300

    Допилены FAQ

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <li><h2>U1 HOST</h2></li>
             <li><a href="#vps-servers">VPS сервера</a></li>
             <li><a href="#">GPT советник</a></li>
-            <li><a href="#">FAQ</a></li>
+            <li><a href="#faq">FAQ</a></li>
             <li><a href="#">Реферальная программа</a></li>
             <li><a href="#">База знаний</a></li>
           </ul>
